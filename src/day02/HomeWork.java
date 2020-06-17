61d3584380adbfd02934bc2a8cfabdde3dd15bce
WJ-98 <88349423@qq.com>
2020-06-17 15:50:09 +0800

    王杰的第一次项目结构上传

--- src/day02/HomeWork.java
+++ src/day02/HomeWork.java
@@ -1,12 +1,22 @@
 package day02;
 
 public class HomeWork {
+
+
     public static void main(String[] args) {
-        //打印九九乘法表
-        System.out.println("打印成功");
+
         //打印等腰三角形
-        System.out.println("打印成功");
-        System.out.println("利用for循环嵌套");
+        for(int i = 0;i<5;i++){//控制行
+            for(int j=0;j<=5-i;j++){//控制空格的数量
+                System.out.print(" ");
+            }
+            for(int k =1;k<=2*i-1;k++){//控制图形的数量
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
     }
 
 
