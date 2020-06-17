5d12ab771d86df31778285cd1f9bac670d6d2197
WJ-98 <88349423@qq.com>
2020-06-17 17:37:08 +0800

    王杰的第一次项目结构上传

--- src/day02/HomeWork02.java
+++ src/day02/HomeWork02.java
@@ -1,5 +1,22 @@
 package day02;
 
 public class HomeWork02 {
+    //求100以内所有质数的和
+    public static void main(String[] args) {
 
+        int sum = 0;
+        for(int i=2;i<=100;i++){
+                int j=2;
+                while(i % j!=0 ){
+                    j++;
+                }
+                if(i==j){
+                    sum+=i;
+                }
+        }
+        System.out.println(sum);
+    }
 }
+
+
+
