043cd9f1a412907ba6331d02c33435e906d7a225
WJ-98 <88349423@qq.com>
2020-06-17 11:40:02 +0800

    王杰的第一次项目结构上传

---
+++ src/day02/HomeWork.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        //打印九九乘法表
+        System.out.println("打印成功");
+        //打印等腰三角形
+        System.out.println("打印成功");
+    }
+
+
+}
